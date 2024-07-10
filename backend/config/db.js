8fc8b0302354dddbf37cc600dc57a5613aad632b
Anquetil-G <128863847+Anquetil-G@users.noreply.github.com>
2024-07-10 10:49:36 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const { ServerApiVersion } = require('mongodb'); // Importation de ServerApiVersion pour configurer le serveur API
+const uri = "mongodb+srv://AnquetilGabin:EdKOvGg4lIQCqC9c@cluster0.honqyvn.mongodb.net/JustTalk";
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
       serverApi: {
